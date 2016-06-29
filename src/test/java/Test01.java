bb8a73cf75dfc52e963e6a2b8c1d9af78c9b5cde
Alex Yang <yu.yang@symbio.com>
2016-06-29 15:38:35 +0800

    yodate

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class Test01 {
@@ -7,11 +9,13 @@ public class Test01 {
     @Test
     public void tesJenkins() throws InterruptedException {
         System.out.println("Test begin!!!");
-        WebDriver driver = new FirefoxDriver();
+        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        WebDriver driver = new RemoteWebDriver(dc);
         driver.get("http://www.baidu.com");
         Thread.sleep(10000);
         driver.get("http://www.google.com");
         Thread.sleep(10000);
         System.out.println("Test over!!!");
+        driver.close();
     }
 }
